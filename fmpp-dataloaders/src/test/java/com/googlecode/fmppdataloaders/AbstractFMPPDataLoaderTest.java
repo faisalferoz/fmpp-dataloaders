55132cb36ce7dff02c96e2a479c8493b17926969
FaisalFeroz <FaisalFeroz@0035d198-0d5f-adbe-be90-805676d655dd>
2010-09-23 16:55:43 +0000

    Changes for deploying to Nexus Maven Repository.

--- fmpp-dataloaders/src/test/java/com/googlecode/fmppdataloaders/AbstractFMPPDataLoaderTest.java
+++ fmpp-dataloaders/src/test/java/com/googlecode/fmppdataloaders/AbstractFMPPDataLoaderTest.java
@@ -21,7 +21,7 @@
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
  * SOFTWARE.
  ******************************************************************************/
-package com.google.code.fmppdataloaders;
+package com.googlecode.fmppdataloaders;
 
 import java.io.File;
 
